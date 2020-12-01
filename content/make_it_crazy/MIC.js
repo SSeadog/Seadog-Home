7f96bd779ac031928e1170efd2f3916733b79cb0
Seadog1225 <aasssa1225@naver.com>
2020-12-01 16:36:57 +0900

    Remake MIC Content(Auto Move)

--- content/make_it_crazy/MIC.js
+++ content/make_it_crazy/MIC.js
@@ -1,20 +1,31 @@
 class App {
   constructor() {
     this.upperCan = document.createElement('canvas');
+    this.upperCan.style.zIndex = 3;
     this.upperCtx = this.upperCan.getContext('2d');
 
     document.body.appendChild(this.upperCan);
 
     this.underCan = document.createElement('canvas');
+    this.underCan.style.position = 'relative';
+    this.underCan.style.bottom = '6px';
     this.underCtx = this.underCan.getContext('2d');
 
     document.body.appendChild(this.underCan);
+
+    this.btn = document.createElement('button');
+    this.btn.innerText = "사진 변경";
+
+    document.body.appendChild(this.btn);
 
     window.addEventListener('resize', this.resize.bind(this), false);
     this.resize();
 
     this.img = document.createElement('img');
     this.img.src = "../../public/default.png";
+
+    this.skew = 0;
+    this.speed = 0.01; // 각도에 따라 값을 달리 줘야할 듯
 
     window.requestAnimationFrame(this.animate.bind(this));
   }
@@ -34,11 +45,18 @@ class App {
   animate(t) {
     window.requestAnimationFrame(this.animate.bind(this));
 
+    this.skew += this.speed;
+    if(this.skew >= .7 || this.skew <= -.7) {
+      this.speed *= -1;
+    }
+
     this.upperCtx.clearRect(0, 0, this.stageWidth, this.stageHeight / 2);
-    // this.upperCtx.setTransform(2, 0, .8, 2, -this.stageWidth * .26, 0);
+    // 3번째 값이랑 5번째 값을 계속 변경시켜야함
+    this.upperCtx.setTransform(2, 0, this.skew, 2, -this.stageWidth * this.skew * .85, 0);
     this.upperCtx.drawImage(this.img, 0, 0, this.img.width, this.img.height / 2, 0, 0, this.stageWidth, this.stageHeight / 2);
 
     this.underCtx.clearRect(0, 0, this.stageWidth, this.stageHeight / 2);
+    this.underCtx.setTransform(2, 0, -this.skew, 2, 0, 0);
     this.underCtx.drawImage(this.img, 0, this.img.height / 2, this.img.width, this.img.height / 2, 0, 0, this.stageWidth, this.stageHeight / 2);
   }
 }
